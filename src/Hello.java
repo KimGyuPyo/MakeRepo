15acc3eaa01877e0cbd8ac628daca0e843728671
user <user@DESKTOP-DU01E7K>
2024-09-06 12:07:49 +0900

    "커밋 테스트입니다."

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Kim Gyu Pyo");
+		System.out.print("20100572 김규표");
+		System.out.println(" Recognize difference between println and print ?");
+	}
+
+}
